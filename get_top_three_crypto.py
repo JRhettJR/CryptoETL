f988e70d27d75bf881e29023e7627185ecfcdafa
JRhettJR <62896206+jrhettjr@users.noreply.github.com>
2022-07-15 22:05:47 -0600

    ADD
        main code
        venv "virtual environment"

--- get_top_three_crypto.py
+++ get_top_three_crypto.py
@@ -1,13 +1,11 @@
 import requests
 
-headers = {
-    'accept': 'application/json',
-}
+headers = {'accept': 'application/json', }
 
-params = {
-    'region': 'US',
-    'lang': 'en',
-    'symbols': 'AAPL,BTC-USD,EURUSD=X',
-}
+params = {'region': 'US',
+          'lang': 'en',
+          'symbols': 'ETH-USD,BTC-USD,ADA-USD', }
 
-response = requests.get('https://yfapi.net/v6/finance/quote', params=params, headers=headers)
+response = requests.get('https://yfapi.net/v6/finance/quote',
+                        params=params,
+                        headers=headers)
